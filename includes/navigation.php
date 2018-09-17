7e676295decf4d67e3d4a0d3e2d16083fdce2e9f
Efua <kagyarek@u.rochester.edu>
2018-09-17 16:01:56 -0400

    started working on nav.inc

---
+++ includes/navigation.php
@@ -0,0 +1,8 @@
+<nav class="menu">
+	<ul>
+		<li><a href="tab-1-intro.php">Intro</a></li>
+		<li><a href="tab-2-the-beginning.php">The Beginning</a></li>
+		<li><a href="tab-3-the-dark.php">The Dark</a></li>
+		<li><a href="tab-4-the-light.php">The Light</a></li>
+	</ul>
+</nav>
