f8d4277d96923e10fc65c9a6ec758a3a823f0e00
Margaret Thurston <mthursto@u.rochester.edu>
2018-09-17 16:33:49 -0400

    did some stuff (i forget)

--- kelly.php
+++ kelly.php
@@ -15,7 +15,7 @@
 
 		<h2>About Kelly</h2>
 
-			<img src="images/me.jpg" alt="kelly by cascade falls" class="falls">
+			<img src="images/kelly.jpg" alt="kelly by cascade falls" class="falls">
 
 			<p>Born and raised in Albany, New York one of three girls from a strong strict catholic household. Graduated high school with honors and then went right to RIT (Rochester Insititute of Technology). Studying for pyschology was something that I didn't want ot do after a year and a half. I moved back home to work at DiBellas and hated it even became a manager in hopes it would be better... you learn a lot working in the food industry. I quit and traveled around the United States came back got my associates degree in Digital Media at Hudson Valley Community College. I transferred to Univeristy of Rochester and have taken a lot of opportunties and continue to prosper within the Digital Media Realm. The great outdoors are also a great escape from all the technology when needed. </p>
 
