d1e89e1ed3d035df6b96da48ddbcb9236ef7e5ae
Margaret Thurston <mthursto@u.rochester.edu>
2018-09-19 20:49:01 -0400

    Made the pictures clickable

--- index.php
+++ index.php
@@ -17,9 +17,42 @@
 
 		<h1>Welcome to our webpages!</h1>
 
-		<img src="images/efua.jpg">
-		<img src="images/graduation.jpg">
-		<img src="images/on_bus.jpg">
+		<a href="http://localhost/Paris-flash-website2/efua.php">
+			<img src="images/efua.jpg" alt="Efua">
+		</a>
+		
+
+		<a href="http://localhost/Paris-flash-website2/kelly.php">
+			<img src="images/graduation.jpg" alt="Kelly">
+		</a>
+		
+
+		<a href="http://localhost/Paris-flash-website2/margaret.php">
+			<img src="images/on_bus.jpg" alt="Margaret">
+		</a>
+
+
+
+
+	<!--
+
+		<figure>
+			<img src="images/efua.jpg">
+			<figcaption>Efua</figcaption>
+		</figure>
+
+		<figure>
+			<img src="images/graduation.jpg">
+			<figcaption>Kelly</figcaption>
+		</figure>
+
+		<figure>
+			<img src="images/on_bus.jpg">
+			<figcaption>Margaret</figcaption>
+		</figure>
+
+	-->
+
 
 	<?php include "inc/scripts.inc"; ?>
 	</body>
