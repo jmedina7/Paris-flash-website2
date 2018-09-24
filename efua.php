b91fc12ff0ecc4ba04e820de5d9c00fc4200f982
famousclark <clark.famous@gmail.com>
2018-09-24 16:35:08 -0400

    semantic ui css containr - famous

--- efua.php
+++ efua.php
@@ -12,35 +12,39 @@
 
 		<?php include ("includes/navigation.inc"); ?>
 
-		<h1>Efua Agyare-Kumi</h1>
+		<div class="ui container">
 
-		<img src="images/efua.jpg">
+			<h1>Efua Agyare-Kumi</h1>
 
-		<h2>About Efua</h2>
+			<img src="images/efua.jpg">
 
-			<p>Storyteller. Community builder. Pan-Africanist. These words describe some of the most important aspects of my being. One word that was up there for a long time and recently fell away was medicine junkie. One word that will be up there soon is changemaker. The ones you see -- and even the ones you don’t -- are all connected:  I tell stories and build communities in order to inspire change, to spark a roaring fire in others. I do not always do this within Africa, but my heart is always calling for the continent, which I consider my home. If this sounds grand, lofty or pretentious, perhaps a glimpse into my background will help.</p>
+			<h2>About Efua</h2>
 
-		<h2>The Beginning</h2>
+				<p>Storyteller. Community builder. Pan-Africanist. These words describe some of the most important aspects of my being. One word that was up there for a long time and recently fell away was medicine junkie. One word that will be up there soon is changemaker. The ones you see -- and even the ones you don’t -- are all connected:  I tell stories and build communities in order to inspire change, to spark a roaring fire in others. I do not always do this within Africa, but my heart is always calling for the continent, which I consider my home. If this sounds grand, lofty or pretentious, perhaps a glimpse into my background will help.</p>
 
-			<p>I was born in England to a Nigerian mother and Ghanaian father, we moved to Ghana when I was 8 years old. By this time, I had already developed a strong love for science and medicine; something which was very easy to fuel among the pre-professional fever of Ghana’s middle class. I studied physics, biology and chemistry (Chemistry was my favorite), and even tried Additional mathematics before realising that I would crumble and die if I left myself with no creative outlet.</p>
+			<h2>The Beginning</h2>
 
-			<p>So I continued to take music classes and to play piano. I was a good student, but my interest in the sciences seldom extended beyond the classroom. All of my extracurriculars were centered around people and the arts: I was the student body vice president, I fundraised for charities and visited small children and hospitals with donations and gifts. I also acted as the Editor-in-Chief for my school’s annual magazine, and organised our 10th anniversary literary and cultural night. These things made me feel alive; but for some reason, my love for them never tore my attention away from the ultimate goal: to be the very first doctor in my family.</p>
+				<p>I was born in England to a Nigerian mother and Ghanaian father, we moved to Ghana when I was 8 years old. By this time, I had already developed a strong love for science and medicine; something which was very easy to fuel among the pre-professional fever of Ghana’s middle class. I studied physics, biology and chemistry (Chemistry was my favorite), and even tried Additional mathematics before realising that I would crumble and die if I left myself with no creative outlet.</p>
 
-		<h2>The Dark Spot</h2>
+				<p>So I continued to take music classes and to play piano. I was a good student, but my interest in the sciences seldom extended beyond the classroom. All of my extracurriculars were centered around people and the arts: I was the student body vice president, I fundraised for charities and visited small children and hospitals with donations and gifts. I also acted as the Editor-in-Chief for my school’s annual magazine, and organised our 10th anniversary literary and cultural night. These things made me feel alive; but for some reason, my love for them never tore my attention away from the ultimate goal: to be the very first doctor in my family.</p>
 
-			<p>And then my father had a stroke. What was heartbreaking was not that this awful thing had happened to him so early, but more so that I knew that in a different country with a better healthcare system, his outcome might have been drastically different. For a long time, all my thoughts regarding hospitals, doctors, and medicine were filled with pain and distrust. As my dad received care from overworked doctors who didn’t have half the equipment or staff they needed to do a decent job, I raged on the inside, repeatedly asking myself: “How did we get here?”</p>
+			<h2>The Dark Spot</h2>
 
-		<h2>The Light at the End</h2>
+				<p>And then my father had a stroke. What was heartbreaking was not that this awful thing had happened to him so early, but more so that I knew that in a different country with a better healthcare system, his outcome might have been drastically different. For a long time, all my thoughts regarding hospitals, doctors, and medicine were filled with pain and distrust. As my dad received care from overworked doctors who didn’t have half the equipment or staff they needed to do a decent job, I raged on the inside, repeatedly asking myself: “How did we get here?”</p>
 
-			<p>My search to answer this question led me directly to politics and government to explain the shortcomings in Ghana’s systems - our health, education, economy. We were so disconnected from it, and yet it was the only thing that could save us. In order to have a shot at changing my country (maybe even my continent), I turned to what I knew best: telling stories.</p>
+			<h2>The Light at the End</h2>
 
-			<p>With the right story, you can equip a person to change the world, and make them want to do it too. And so I have made it my mission to ensure that all young Ghanaians have the information they need to be engaged and active in their country’s development. That’s the reason I do everything: to learn new and better ways to tell important stories that people will listen to, and that will inspire them to act.</p>
+				<p>My search to answer this question led me directly to politics and government to explain the shortcomings in Ghana’s systems - our health, education, economy. We were so disconnected from it, and yet it was the only thing that could save us. In order to have a shot at changing my country (maybe even my continent), I turned to what I knew best: telling stories.</p>
 
-		<footer>
-			Source: <a href="http://csc174.org/assignment02/eagyare-kumi/tab-1-intro.php">Efua's lab 2</a> on CSC174.org
-		</footer>
+				<p>With the right story, you can equip a person to change the world, and make them want to do it too. And so I have made it my mission to ensure that all young Ghanaians have the information they need to be engaged and active in their country’s development. That’s the reason I do everything: to learn new and better ways to tell important stories that people will listen to, and that will inspire them to act.</p>
 
-	<?php include "includes/scripts.inc"; ?>
+			<footer>
+				Source: <a href="http://csc174.org/assignment02/eagyare-kumi/tab-1-intro.php">Efua's lab 2</a> on CSC174.org
+			</footer>
+
+			<?php include "includes/scripts.inc"; ?>
+
+		</div>
 	</body>
 
 </html>
--- index.php
+++ index.php
@@ -13,25 +13,30 @@
 
 		<?php include ("includes/navigation.inc"); ?>
 
-		<h1>Profiles of Future Web Designers</h1>
-
-		<a href="http://localhost/Paris-flash-website2/efua.php">
-			<img src="images/efua.jpg" alt="Efua">
-		</a>
-
-		<a href="http://localhost/Paris-flash-website2/kelly.php">
-			<img src="images/kelly_graduation.jpg" alt="Kelly">
-		</a>
-
-		<a href="http://localhost/Paris-flash-website2/margaret.php">
-			<img src="images/margaret_on_bus.jpg" alt="Margaret">
-		</a>
+		<div class="ui container">
 
 
-		<h2>Who are the people studying web design?</h2>
+			<h1>Profiles of Future Web Designers</h1>
+
+			<a href="http://localhost/Paris-flash-website2/efua.php">
+				<img src="images/efua.jpg" alt="Efua">
+			</a>
+
+			<a href="http://localhost/Paris-flash-website2/kelly.php">
+				<img src="images/kelly_graduation.jpg" alt="Kelly">
+			</a>
+
+			<a href="http://localhost/Paris-flash-website2/margaret.php">
+				<img src="images/margaret_on_bus.jpg" alt="Margaret">
+			</a>
 
 
-		<P>As technology continues to become increasingly vital to our society, more people from all fields are choosing to study web design. An political activist, a digital scholar, and an enviornmental economist respectively, Efua Margret and Kelly  exemplify this trend. Learn more about their backgrounds and their passions, and discover how they all came to study web design at the University of Rochester. </P>
+			<h2>Who are the people studying web design?</h2>
+
+
+			<P>As technology continues to become increasingly vital to our society, more people from all fields are choosing to study web design. An political activist, a digital scholar, and an enviornmental economist respectively, Efua Margret and Kelly  exemplify this trend. Learn more about their backgrounds and their passions, and discover how they all came to study web design at the University of Rochester. </P>
+
+		</div>
 
 	<?php include "includes/scripts.inc"; ?>
 	</body>
