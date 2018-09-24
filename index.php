80c23dc02cdcabc37f461c1540fd4fd0004f3454
dbacker2 <43354109+dbacker2@users.noreply.github.com>
2018-09-24 16:20:53 -0400

    reformating index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Our Websites</title>
+		<title>Future Web Designers</title>
 
 		<?php include "includes/html-top.inc"; ?>
 
@@ -13,7 +13,7 @@
 
 		<?php include ("includes/navigation.inc"); ?>
 
-		<h1>Welcome to our webpages!</h1>
+		<h1>Profiles of Future Web Designers</h1>
 
 		<a href="http://localhost/Paris-flash-website2/efua.php">
 			<img src="images/efua.jpg" alt="Efua">
@@ -27,6 +27,11 @@
 			<img src="images/margaret_on_bus.jpg" alt="Margaret">
 		</a>
 
+
+		<h2>Who are the people studying web design?</h2>
+
+
+		<P>As technology continues to become increasingly vital to our society, more people from all fields are choosing to study web design. An political activist, a digital scholar, and an enviornmental economist respectively, Efua Margret Kelly and Margaret exemplify this trend. Learn more about their backgrounds and their passions, and discover how they all came to study web design at the University of Rochester. </P>
 
 	<?php include "inc/scripts.inc"; ?>
 	</body>
