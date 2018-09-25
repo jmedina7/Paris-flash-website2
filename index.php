c3e1cc2631661d31e5342e196b2456e33660317c
famousclark <clark.famous@gmail.com>
2018-09-24 22:45:48 -0400

    css edit, fixing container - famous

--- index.php
+++ index.php
@@ -12,43 +12,36 @@
 	<body>
 		<header>
 
-		<?php include ("includes/navigation.inc"); ?>
+			<?php include ("includes/navigation.inc"); ?>
 
-		<div class="ui container">
+		</header>
+
+		<div class="ux">
 
 
 			<h1>Profiles of Future Web Designers</h1>
 
 			<a href="http://localhost/Paris-flash-website2/efua.php">
-				<img src="images/efua.jpg" alt="Efua">
+				<img src="images/efua.jpg" alt="Efua"/>
 			</a>
 
 
 			<a href="http://localhost/Paris-flash-website2/kelly.php">
-				<img src="images/kelly_graduation.jpg" alt="Kelly">
+				<img src="images/kelly_graduation.jpg" alt="Kelly"/>
 			</a>
-
-		</header>
-
-		</header>
-
 
 
 			<a href="http://localhost/Paris-flash-website2/margaret.php">
-				<img src="images/margaret_on_bus.jpg" alt="Margaret">
+				<img src="images/margaret_on_bus.jpg" alt="Margaret"/>
 			</a>
 
 
 			<h2>Who are the people studying web design?</h2>
 
 
-			<P>As technology continues to become increasingly vital to our society, more people from all fields are choosing to study web design. An political activist, a digital scholar, and an enviornmental economist respectively, Efua Margret and Kelly  exemplify this trend. Learn more about their backgrounds and their passions, and discover how they all came to study web design at the University of Rochester. </P>
+			<p>As technology continues to become increasingly vital to our society, more people from all fields are choosing to study web design. An political activist, a digital scholar, and an enviornmental economist respectively, Efua Margret and Kelly  exemplify this trend. Learn more about their backgrounds and their passions, and discover how they all came to study web design at the University of Rochester. </p>
 
-<<<<<<< HEAD
 		</div>
-=======
-		<P>As technology continues to become increasingly vital to our society, more people from all fields are choosing to study web design. As a political activist, a digital scholar, and an enviornmental economist respectively, Efua Margret and Kelly  exemplify this trend. Learn more about their backgrounds and their passions, and discover how they all came to study web design at the University of Rochester. </P>
->>>>>>> e1836b560333c066d92a3e86caf4c80a7fa58663
 
 	<?php include "includes/scripts.inc"; ?>
 	</body>
--- kelly.php
+++ kelly.php
@@ -9,36 +9,34 @@
 
 	<body>
 		<header>
-		<?php include ("includes/navigation.inc"); ?>
+
+			<?php include ("includes/navigation.inc"); ?>
+
+		</header>
 
 
 		<div class="ui container">
 
 			<h1>Kelly Grace Thornton</h1>
 
-		<h1>Kelly Grace Thornton</h1>
-		</header>
 			<img src="images/kelly_graduation.jpg" alt="kelly" class="falls">
-
-
-				<img src="images/kelly_graduation.jpg" alt="kelly" class="falls">
 
 			<h2>About Kelly</h2>
 
-				<p>Born and raised in Albany, New York one of three girls from a strong strict catholic household. Graduated high school with honors and then went right to RIT (Rochester Insititute of Technology). Studying for pyschology was something that I didn't want ot do after a year and a half. I moved back home to work at DiBellas and hated it even became a manager in hopes it would be better... you learn a lot working in the food industry. I quit and traveled around the United States came back got my associates degree in Digital Media at Hudson Valley Community College. I transferred to Univeristy of Rochester and have taken a lot of opportunties and continue to prosper within the Digital Media Realm. The great outdoors are also a great escape from all the technology when needed. </p>
+			<p>Born and raised in Albany, New York one of three girls from a strong strict catholic household. Graduated high school with honors and then went right to RIT (Rochester Insititute of Technology). Studying for pyschology was something that I didn't want ot do after a year and a half. I moved back home to work at DiBellas and hated it even became a manager in hopes it would be better... you learn a lot working in the food industry. I quit and traveled around the United States came back got my associates degree in Digital Media at Hudson Valley Community College. I transferred to Univeristy of Rochester and have taken a lot of opportunties and continue to prosper within the Digital Media Realm. The great outdoors are also a great escape from all the technology when needed. </p>
 
 			<h2>Early Life</h2>
 
-				<p>Colonie, New York raised in a tight knit family. 18 months old I was diagnosed with being profoundly deaf. I am one of six people that are Deaf within my family. My maternal grandparents, as well as my mother and younger sister. I was always ambitous from the start and when I was young I wanted to be a rock star. Growing up in a strict household made us a really tight knit family between cousins. I graduated high school in 2010 and started school at RIT. Dropped out, worked for four long grueling years and realized I wanted to do more. Travelled around the United States, came back and went back to school. Thats where she began her journey into the digital media world.</p>
+			<p>Colonie, New York raised in a tight knit family. 18 months old I was diagnosed with being profoundly deaf. I am one of six people that are Deaf within my family. My maternal grandparents, as well as my mother and younger sister. I was always ambitous from the start and when I was young I wanted to be a rock star. Growing up in a strict household made us a really tight knit family between cousins. I graduated high school in 2010 and started school at RIT. Dropped out, worked for four long grueling years and realized I wanted to do more. Travelled around the United States, came back and went back to school. Thats where she began her journey into the digital media world.</p>
 
 
 			<h2>Hudson Valley</h2>
 
-				<p>January of 2015 I entered Hudson Valley Community College. I had started their digital media associates program. The program is only about five years old so it is a new subject. This major was not available when I initially graduated high school in 2010. I was able to build my resume with skill and was able to build a portfolio. This made me realize my love for web art, and digital art. I graduated with honors and was on the President's list the whole time I was a student there. Learning that art was the best way to help Deaf community I found my passion within the digital world to convey the messages that I thinks need to be spoken about. I focused on social issues that we all face hearing or deaf. Then I emulated a few artists but using Deaf world perspectives. Then towards the end of my time at Hudson Valley I focused more so on the form and aesethic of sign language. Also at the end I knew I wanted to pursue more of my education.</p>
+			<p>January of 2015 I entered Hudson Valley Community College. I had started their digital media associates program. The program is only about five years old so it is a new subject. This major was not available when I initially graduated high school in 2010. I was able to build my resume with skill and was able to build a portfolio. This made me realize my love for web art, and digital art. I graduated with honors and was on the President's list the whole time I was a student there. Learning that art was the best way to help Deaf community I found my passion within the digital world to convey the messages that I thinks need to be spoken about. I focused on social issues that we all face hearing or deaf. Then I emulated a few artists but using Deaf world perspectives. Then towards the end of my time at Hudson Valley I focused more so on the form and aesethic of sign language. Also at the end I knew I wanted to pursue more of my education.</p>
 
 			<h2>University of Rochester</h2>
 
-				<p>After graduating Hudson Valley Community College, I applied to three schools. University of Rochester was the first to accept me. I felt like it was necessary for me to go to University of Rochester so that I could expose more hearing people of the deaf culture and make them interact. It is interesting to see other perspectives within the digital media culture and expand more so on that. As someone who is visual and wants to import that into the world and have that be a way of accessibility it is something that drives me in finding out the ways I can do that. Digitally I feel like there is much potential and using the digital media as well as learning some computer science will leave me ill equipped for what I would like to pursue after University of Rochester. Moving to Rochester has brought a lot of new things to me as well as my education. I also play for the Rochester women's rugby club team. New things can bring you to new invigorating experiences.
+			<p>After graduating Hudson Valley Community College, I applied to three schools. University of Rochester was the first to accept me. I felt like it was necessary for me to go to University of Rochester so that I could expose more hearing people of the deaf culture and make them interact. It is interesting to see other perspectives within the digital media culture and expand more so on that. As someone who is visual and wants to import that into the world and have that be a way of accessibility it is something that drives me in finding out the ways I can do that. Digitally I feel like there is much potential and using the digital media as well as learning some computer science will leave me ill equipped for what I would like to pursue after University of Rochester. Moving to Rochester has brought a lot of new things to me as well as my education. I also play for the Rochester women's rugby club team. New things can bring you to new invigorating experiences.
 				</p>
 
 			<footer>
@@ -47,7 +45,7 @@
 
 		</div>
 
-	<?php include "includes/scripts.inc"; ?>
+		<?php include "includes/scripts.inc"; ?>
 	</body>
 
 </html>
