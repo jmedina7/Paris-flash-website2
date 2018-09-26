763238816399680ba414ed228640d0a8b734ae54
famousclark <clark.famous@gmail.com>
2018-09-26 16:26:23 -0400

    Active navigation - famous

--- js/activeNav.js
+++ js/activeNav.js
@@ -1,11 +1,12 @@
 var setActive = () => {
   this.console.log('this printed');
 
-  var nav = document.getElementById('nav').getElementsByTagName('li');
+  var nav = document.getElementById('nav').getElementsByTagName('a');
   this.console.log(nav);
 
   for(var index=0; index< nav.length; index++) {
     if(document.location.href.indexOf(nav[index].href)>=0) {
+      this.console.log(nav[index]);
       nav[index].className='active item';
     }
   }
